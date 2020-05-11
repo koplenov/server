3bd1a042cfae01b2c13b56768e2bf0c44049df6c
koplenov <koplenov@gmail.com>
2020-05-11 19:07:29 +0300

    s

--- server.go
+++ server.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"github.com/umahmood/haversine"
 	"log"
 	"net/http"
+	"os"
 	"strings"
 )
 
-var id string
-var ic string
-var is string
+
+var mcc string
+var mnc string
+var lac string
+var cellid string
 
 // amd64 386
 func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
@@ -20,22 +24,39 @@ func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
 	//fmt.Println("scheme", r.URL.Scheme)
 	//fmt.Println(r.Form["url_long"])
 
+	if len(r.Form) != 4 { // 4 параметра :з
+		fmt.Fprintf(w, "error 91237")
+		return
+	}	else {
+		fmt.Fprintf(w, "ok")
+	}
+
+	// key - value
 	for k, v := range r.Form {
 		switch {
-		case k == "id":
-			fmt.Println(k, strings.Join(v, ""))
-			///fmt.Println("id:", k)
-			///fmt.Println("val:", strings.Join(v, ""))
-		case k == "lat":
-			fmt.Println("lat:", k)
-			fmt.Println("val:", strings.Join(v, ""))
-		case k == "len":
-			fmt.Println("len:", k)
-			fmt.Println("val:", strings.Join(v, ""))
+		case k == "mcc":
+			mcc = strings.Join(v, "")
+			//fmt.Println(k, strings.Join(v, ""))
+		case k == "mnc":
+			mnc = strings.Join(v, "")
+			//fmt.Println("lat:", k)
+			//fmt.Println("val:", strings.Join(v, ""))
+		case k == "lac":
+			lac = strings.Join(v, "")
+			//fmt.Println("len:", k)
+			//fmt.Println("val:", strings.Join(v, ""))
+		case k == "cellid":
+			cellid = strings.Join(v, "")
+			//fmt.Println("len:", k)
+			//fmt.Println("val:", strings.Join(v, ""))
 		}
-
 		///fmt.Println("key:", k)
 		///fmt.Println("val:", strings.Join(v, ""))
+
+
+
+
+
 	}
 	fmt.Fprintf(w, "ok") // отправляем данные на клиентскую сторону
 	println("next")
@@ -47,6 +68,12 @@ func main() {
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+
+	empData := [][]string{
+		{"Name", "City", "Skills"},
+	}
+	println(empData)
+	print("asdasd")
 }
 
 func mainc() {
@@ -59,4 +86,23 @@ func mainc() {
 	turin := haversine.Coord{Lat: 45.04, Lon: 7.42}                     // Turin, Italy
 	mi, km := haversine.Distance(oxford, turin)
 	fmt.Println("Miles:", mi, "Kilometers:", km)
+}
+
+func test(id string, mcc string, mnc string, lac string, cellid string,) {
+	csvFile, err := os.Create(id + ".csv")
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+
+	csvwriter := csv.NewWriter(csvFile)
+
+	empData := [][]string{
+		{"Name", "City", "Skills"},
+	}
+
+	for _, empRow := range empData {
+		_ = csvwriter.Write(empRow)
+	}
+	csvwriter.Flush()
+	csvFile.Close()
 }
